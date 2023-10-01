5411b06c058108e707f43e812a91ce2d7e4258c4
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-01 10:01:07 +0500

    Todo app components and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,50 @@
+import { useState, ChangeEvent } from 'react';
+
+import styles from './home.module.css';
+import { IData } from './interfaces/index.js';
+
 const App = (): JSX.Element => {
-  return <div>App</div>;
+  const data: IData[] = [
+    { title: 'Omar', id: 1, description: 'Description' },
+    { title: 'Osman', id: 1, description: 'Description' },
+    { title: 'Abdulloh', id: 1, description: 'Description' },
+  ];
+
+  const [title, setTitle] = useState<string>();
+  const [arr, setArr] = useState<IData[]>(data);
+
+  const changeHandler = (evt: ChangeEvent<HTMLInputElement>): void => {
+    setTitle(evt.target.value);
+  };
+
+  const handleSubmit = (): void => {
+    if (!title?.length) return;
+    setTitle('');
+  };
+
+  return (
+    <div className={styles.todo}>
+      <h1 className={styles.title}>APP Todo</h1>
+
+      <input
+        placeholder="Enter todo"
+        value={title}
+        onChange={changeHandler}
+        className={styles.input}
+      />
+      <button onClick={handleSubmit} className={styles.button}>
+        Add Todo
+      </button>
+
+      <div className={styles.card}>
+        {arr.map((c) => (
+          <div key={c.id} className={styles.cardItem}>
+            <p>{c.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
